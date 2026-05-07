4204155d4674d938f62908a7f73f28ed0b852b5a
epicactsuperplayer <winpp826@gmail.com>
2026-05-07 17:54:05 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,18 @@
+# Brawl Stats Vercel Setup
+
+1. Upload everything to GitHub
+
+2. Import project into Vercel
+
+3. Add Environment Variable:
+
+Name:
+BRAWL_TOKEN
+
+Value:
+YOUR_BRAWL_STARS_API_KEY
+
+4. Redeploy
+
+IMPORTANT:
+Your Brawl Stars API key must allow Vercel server IPs.
---
+++ battlelog.js
@@ -0,0 +1,26 @@
+export default async function handler(req, res) {
+  try {
+    const tag = req.query.tag;
+
+    if (!tag) {
+      return res.status(400).json({ error: 'Missing tag' });
+    }
+
+    const cleanTag = tag.replace('#', '');
+
+    const response = await fetch(
+      `https://api.brawlstars.com/v1/clubs/%23${cleanTag}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.BRAWL_TOKEN}`
+        }
+      }
+    );
+
+    const data = await response.json();
+
+    res.status(response.status).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
